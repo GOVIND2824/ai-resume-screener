446013649df7f04b70b508bc1f16d5b407635d2b
Govind Aggarwal <148374412+GOVIND2824@users.noreply.github.com>
2026-02-11 17:51:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 from pypdf import PdfReader
 
 def extract_text_from_pdf(file):
     reader = PdfReader(file)
     text = ""
     for page in reader.pages:
-        text += page.extract_text()
+        text += page.extract_text() or ""
     return text
-=======
-from pypdf import PdfReader
-
-def extract_text_from_pdf(file):
-    reader = PdfReader(file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
->>>>>>> 19ea6d2c8a3837cd04c6a11358f9b8563a5695b5
